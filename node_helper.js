98f7ce36d4a7972720572206607c910abc17f23e
Dr.Sudhamoy Debbarma <65818001+sdmydbr9@users.noreply.github.com>
2025-01-18 16:31:10 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -10,54 +10,55 @@ const NodeHelper = require('node_helper');
 const spawn = require('child_process').spawn;
 
 module.exports = NodeHelper.create({
-
-    start: function() {
-        console.log("Starting module: " + this.name);
+    start: function () {
+        // Module initialization
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: function (notification, payload) {
         if (notification === "CONFIG") {
-			this.config = payload;
-			this.getData();
+            this.config = payload;
+            this.getData();
         }
     },
-	
-	/**
-	 * getData
-	 * Request data from the supplied URL and broadcast it to the MagicMirror module if it's received.
-	 */
-    getData: function() {
-		if (this.readableStream && this.readableStream != null){
-			// Do not start multiple processes
-			return;
-		}
-		
-		const self = this;
-		self.str_payload = "";
-		
-		this.readableStream = spawn(__dirname + "/shairport-metadata.sh", [this.config.metadataPipe, __dirname]);
-		this.readableStream.stderr.on('data', function(payload){
-			console.log("ERR: ", payload.toString());
-		});
-		
-		this.readableStream.on('error', function(err){
-			console.log('an error occurred: ', err.toString());
-		});
-		
-		this.readableStream.stdout.on('data', function(payload){
-			self.str_payload += payload.toString();
-			var lines = self.str_payload.split('\n')
-			self.str_payload = lines.pop();
-			for(var i = 0; i < lines.length; i++){
-				if (lines[i].length == 0){ continue; }
-				self.sendSocketNotification("DATA", JSON.parse(lines[i]));
-			}
-		});
-		
-		this.readableStream.stdout.on('close', function(){
-			self.readableStream.kill();
-			self.readableStream = null;
-		})
-    }
-	
+
+    getData: function () {
+        if (this.readableStream && this.readableStream !== null) {
+            return; // Prevent multiple processes
+        }
+
+        const self = this;
+        self.str_payload = "";
+
+        this.readableStream = spawn(__dirname + "/shairport-metadata.sh", [this.config.metadataPipe, __dirname]);
+
+        this.readableStream.stderr.on('data', function () {
+            // Handle stderr if necessary
+        });
+
+        this.readableStream.on('error', function () {
+            // Handle errors in the readable stream
+        });
+
+        this.readableStream.stdout.on('data', function (payload) {
+            self.str_payload += payload.toString();
+            const lines = self.str_payload.split('\n');
+            self.str_payload = lines.pop();
+
+            for (let i = 0; i < lines.length; i++) {
+                if (lines[i].length === 0) continue;
+
+                try {
+                    const data = JSON.parse(lines[i]);
+                    self.sendSocketNotification("DATA_BROADCAST", data);
+                } catch (err) {
+                    // Handle JSON parse errors
+                }
+            }
+        });
+
+        this.readableStream.stdout.on('close', function () {
+            self.readableStream.kill();
+            self.readableStream = null;
+        });
+    },
 });
