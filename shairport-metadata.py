d1c779a6b0dbfdce07d9be504b17d5552aa2a4fe
Dr.Sudhamoy Debbarma <65818001+sdmydbr9@users.noreply.github.com>
2025-01-19 16:20:49 +0530

    Update shairport-metadata.py to support play pause

--- shairport-metadata.py
+++ shairport-metadata.py
@@ -12,21 +12,13 @@ def start_item(line):
     return (typ, code, length)
 
 def start_data(line):
-    try:
-        assert line == '<data encoding="base64">\n'
-    except AssertionError:
-        if line.startswith("<data"):
-            return 0
-        return -1
-    return 0
+    if line.startswith("<data encoding=\"base64\">"):
+        return 0
+    return -1
 
 def read_data(line, length):
     b64size = 4 * ((length + 2) // 3)
-    try:
-        data = base64.b64decode(line[:b64size])
-    except TypeError:
-        data = b""
-    return data
+    return base64.b64decode(line[:b64size])
 
 def guessImageMime(magic):
     if magic.startswith(b'\xff\xd8'):
@@ -38,10 +30,9 @@ def guessImageMime(magic):
 
 if __name__ == "__main__":
     metadata = {}
-    fi = sys.stdin
     while True:
         line = sys.stdin.readline()
-        if not line:  # EOF
+        if not line:
             break
         sys.stdout.flush()
         if not line.startswith("<item>"):
@@ -55,7 +46,6 @@ if __name__ == "__main__":
                 continue
             data = read_data(sys.stdin.readline(), length)
 
-        # Everything read
         if typ == "core":
             if code == "asal":
                 metadata['Album Name'] = data.decode('utf-8')
@@ -64,21 +54,14 @@ if __name__ == "__main__":
             elif code == "minm":
                 metadata['Title'] = data.decode('utf-8')
 
-        if typ == "ssnc" and code == "pfls":
-            metadata = {}
-            print(json.dumps({}))
+        if typ == "ssnc" and code == "paus":
+            print(json.dumps({"event": "Pause"}))
             sys.stdout.flush()
-        if typ == "ssnc" and code == "pend":
-            metadata = {}
-            print(json.dumps({}))
+
+        if typ == "ssnc" and code == "pres":
+            print(json.dumps({"event": "Resume"}))
             sys.stdout.flush()
-        if typ == "ssnc" and code == "PICT":
-            if len(data) == 0:
-                print(json.dumps({"image": ""}))
-            else:
-                mime = guessImageMime(data)
-                print(json.dumps({"image": "data:" + mime + ";base64," + base64.b64encode(data).decode('utf-8')}))
-            sys.stdout.flush()
+
         if typ == "ssnc" and code == "mden":
             print(json.dumps(metadata))
             sys.stdout.flush()
